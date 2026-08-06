ce3544a3e53815b059d27ec0ebed28e801156924
kushwant Singh Rajawat <168860456+khushwant-Singh-Rajawat@users.noreply.github.com>
2026-08-06 10:08:45 +0530

    Time: 1 ms (99.95%), Space: 46.9 MB (65.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,29 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-       for(int i = 0; i < nums.length; i++) 
-        {
-         result = result ^ nums[i];
-        }
+      
+        for(int i = 0; i < nums.length; i++) { 
+            result ^= nums[i];
+        } 
         return result;
+
+
+    //    for(int i = 0; i < nums.length; i++) 
+    //    {
+    //      int count = 0;
+    //      for(int j = 0; j < nums.length; j++)
+    //      {
+    //          if(nums[i] == nums[j]) 
+    //          { 
+    //             count++;
+    //          }
+    //      }
+         
+    //      if(count == 1) 
+    //      {
+    //          return nums[i];
+    //      } 
+    //     } 
+    // return -1; // safety
     }
 }
