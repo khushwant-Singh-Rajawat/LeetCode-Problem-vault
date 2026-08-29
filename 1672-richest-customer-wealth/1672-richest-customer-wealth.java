e56d9f2b971575ff809669c86948eb85a714d36b
kushwant Singh Rajawat <168860456+khushwant-Singh-Rajawat@users.noreply.github.com>
2026-08-29 09:20:59 +0530

    Time: 0 ms (100%), Space: 44.4 MB (73.96%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -2,12 +2,12 @@ class Solution {
     public int maximumWealth(int[][] accounts)
      {
         int max=0;
-      for(int i=0;i<accounts.length;i++)
+      for(int i=0;i<accounts.length;i++) // i loop for Number of rows/customers
       {
         int sum=0;
-        for(int j=0;j<accounts[i].length;j++)
+        for(int j=0;j<accounts[i].length;j++) // j loop for Number of columns/accounts
         {
-            sum +=accounts[i][j];
+            sum +=accounts[i][j]; // sum for each customer
         } // end of j loop
         if(sum>max)
         max=sum;
